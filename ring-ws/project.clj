edf0dc141c8018cb464a6f8280c5eb9f0ce2b4b7
Takashi AOKI <federkasten@me.com>
2014-09-21 08:15:20 -0700

    add ring-ws project

---
+++ ring-ws/project.clj
@@ -0,0 +1,16 @@
+(defproject ring-jetty/ring-ws "0.1.0-SNAPSHOT"
+  :description "Ring websocket utilities based on jetty."
+  :url ""
+  :scm {:dir ".."}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.2.3.v20140905"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.2.3.v20140905"]]
+  :profiles
+  {:provided {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]}
+   :dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]}
+   :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
---
+++ ring-ws/src/ring_jetty/util/ws.clj
@@ -0,0 +1,52 @@
+(ns ring-jetty.util.ws
+  (:import (org.eclipse.jetty.websocket.api WebSocketAdapter)
+           (java.nio ByteBuffer)
+           (clojure.lang IFn)))
+
+(defprotocol WebSocketProtocol
+  (send! [this msg])
+  (close! [this])
+  (remote-addr [this])
+  (idle-timeout! [this ms]))
+
+(defprotocol WebSocketSend
+  (-send! [x ws] "How to encode content sent to the WebSocket clients"))
+
+(extend-protocol WebSocketSend
+
+  (Class/forName "[B")
+  (-send! [ba ws]
+    (-send! (ByteBuffer/wrap ba) ws))
+
+  ByteBuffer
+  (-send! [bb ws]
+    (-> ws .getRemote (.sendBytes ^ByteBuffer bb)))
+
+  String
+  (-send! [s ws]
+    (-> ws .getRemote (.sendString ^String s)))
+
+  IFn
+  (-send! [f ws]
+    (-> ws .getRemote f))
+
+  Object
+  (-send! [this ws]
+    (-> ws .getRemote (.sendString (str this))))
+
+  ;; "nil" could PING?
+  ;; nil
+  ;; (-send! [this ws] ()
+
+  )
+
+(extend-protocol WebSocketProtocol
+  WebSocketAdapter
+  (send! [this msg]
+    (-send! msg this))
+  (close! [this]
+    (.. this (getSession) (close)))
+  (remote-addr [this]
+    (.. this (getSession) (getRemoteAddress)))
+  (idle-timeout! [this ms]
+    (.. this (getSession) (setIdleTimeout ^long ms))))
